16a2f1f4d2c17e3d428c79b3ae390ee2e6044cc3
camisnew <camilabertholdpires@gmail.com>
2020-11-13 12:26:13 -0300

    base for project

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var fs = require('fs');
+var Papa = require('papaparse');
+var app = express();
+
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  const products = fs.readFileSync('./service/products.csv', 'utf-8');
+  const productsJson = Papa.parse(products, {
+      delimiter: ";",
+      header: true
+    });
+  return response.json(productsJson.data);
+})
+
+app.listen(3333);
